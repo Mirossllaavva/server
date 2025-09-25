2cddf93404dfd0499c69180cc229c18f0c380f80
Mirossllaavva <miroslavahavrilenko@gmail.com>
2025-09-25 09:39:25 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
-import socket
-server_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(("localhost", 7754))
-server_socket.listen(1)
-print("Сервер очікує клієнта")
-connection, address = server_socket.accept()
-print(f"Підключення клієнта: {address}"  )
+import socket #підклюення модуля
+server_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM) #створення об'єкта
+server_socket.bind(("localhost", 7754)) #прив'язуємо до нього IP-аресу 
+server_socket.listen(1) #сервер очікує клієнта 
+print("Сервер очікує клієнта") 
+connection, address = server_socket.accept() #сервер приймає клієнта
+print(f"Підключення клієнта: {address}"  ) 
