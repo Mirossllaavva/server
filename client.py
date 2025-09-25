28a7eb56e7781d5db5ca8d8500c6347b5cfe36b3
Mirossllaavva <miroslavahavrilenko@gmail.com>
2025-09-25 11:18:09 +0300

    lesson2

---
+++ client.py
@@ -0,0 +1,6 @@
+import socket 
+client_socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
+client_socket.connect(("localhost", 7754))
+command = input("Введіть запрос: ")
+client_socket.send(command.encode())
+print(client_socket.recv(1024).decode())
--- server.py
+++ server.py
@@ -1,7 +1,15 @@
 import socket #підклюення модуля
 server_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM) #створення об'єкта
-server_socket.bind(("localhost", 7754)) #прив'язуємо до нього IP-аресу 
+server_socket.bind(("localhost", 7754)) #прив'язуємо до певної адреси в інтернеті(хост)
 server_socket.listen(1) #сервер очікує клієнта 
 print("Сервер очікує клієнта") 
 connection, address = server_socket.accept() #сервер приймає клієнта
 print(f"Підключення клієнта: {address}"  ) 
+while 1:
+    try:
+        command = connection.recv(1024).decode()
+        if command == "2 + 2":
+            connection.send("4".encode())
+        
+    except:
+        pass
