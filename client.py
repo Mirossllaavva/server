2cf6eb8ac9b202c64bb076d530c3826428193cc9
Mirossllaavva <miroslavahavrilenko@gmail.com>
2025-10-01 23:28:38 +0300

    client.py

--- client.py
+++ client.py
@@ -1,6 +1,8 @@
 import socket 
 client_socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(("localhost", 7754))
-command = input("Введіть запрос: ")
-client_socket.send(command.encode())
-print(client_socket.recv(1024).decode())
+while 1:
+  command = input("Введіть запрос: ")
+  client_socket.send(command.encode())
+  
+  print(client_socket.recv(1024).decode())
