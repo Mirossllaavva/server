7835616e6783f4ab9fad5deef15e88312315b900
Mirossllaavva <miroslavahavrilenko@gmail.com>
2025-10-02 10:41:59 +0300

    Add files via upload

--- server.py
+++ server.py
@@ -1,15 +1,35 @@
 import socket #підклюення модуля
+import threading 
 server_socket =  socket.socket(socket.AF_INET, socket.SOCK_STREAM) #створення об'єкта
 server_socket.bind(("localhost", 7754)) #прив'язуємо до певної адреси в інтернеті(хост)
-server_socket.listen(1) #сервер очікує клієнта 
+connect_client = []
+server_socket.listen(5) #сервер очікує клієнта 
+server_socket.setblocking(0)
 print("Сервер очікує клієнта") 
-connection, address = server_socket.accept() #сервер приймає клієнта
-print(f"Підключення клієнта: {address}"  ) 
+def accept_sms():
+    while 1:
+        try:
+            for client in connect_client:
+                message = client.recv(1024).decode()
+                print(message)
+        except: 
+            pass
+threading.Thread(target=accept_sms).start()
+
 while 1:
     try:
-        command = connection.recv(1024).decode()
-        if command == "2 + 2":
-            connection.send("4".encode())
         
+        connection, address = server_socket.accept()
+        connection.setblocking(0)
+        connect_client.append(connection)
+        print(f"Підключення клієнта: {address}"  ) 
     except:
         pass
+# while 1:
+#     try:
+#         command = connection.recv(1024).decode()
+#         if command == "2 + 2":
+#             connection.send("4".encode())
+        
+#     except:
+#         pass
