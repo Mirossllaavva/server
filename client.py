6ec191d7e555e5b74bb73a224175c2917ed492ac
Mirossllaavva <miroslavahavrilenko@gmail.com>
2025-10-02 10:43:06 +0300

    Add files via upload

--- client.py
+++ client.py
@@ -1,8 +1,15 @@
 import socket 
+import threading 
 client_socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(("localhost", 7754))
+def send_sms():
+  while 1:
+    client_message = input("Введіть запрос: ")
+    client_socket.send(client_message.encode())
+threading.Thread(target = send_sms ).start()
 while 1:
-  command = input("Введіть запрос: ")
-  client_socket.send(command.encode())
-  
-  print(client_socket.recv(1024).decode())
+  try:
+    
+    print(client_socket.recv(1024).decode())
+  except:
+    pass
