bc46072bde14b24a28de3c627e506d4c04cf8081
Mirossllaavva <miroslavahavrilenko@gmail.com>
2025-11-22 09:50:47 +0200

    Improve readability and add comments in client.py
    Refactor client.py for better readability and comments.

--- client.py
+++ client.py
@@ -1,15 +1,16 @@
 import socket 
-import threading 
-client_socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect(("localhost", 7754))
-def send_sms():
+import threading #підключення бібліотек
+client_socket = socket.socket (socket.AF_INET, socket.SOCK_STREAM) #створення кліенту
+client_socket.connect(("localhost", 7754)) #підключення його то локального хоста
+def send_sms(): # створюмємо функціяю для відправки соо
   while 1:
-    client_message = input("Введіть запрос: ")
-    client_socket.send(client_message.encode())
-threading.Thread(target = send_sms ).start()
+    client_message = input("Введіть запрос: ") #вводимо смс
+    client_socket.send(client_message.encode()) #кодуємо та відправляємо
+threading.Thread(target = send_sms ).start() #створюємо поток
 while 1:
   try:
     
-    print("\nВідповідь від серверу", client_socket.recv(1024).decode())
+    print("\nВідповідь від серверу", client_socket.recv(1024).decode()) #розкодуємо та виводимо повідомлення від серверу
   except:
+
     pass
